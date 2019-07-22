90bf279720506764daee2c622457edf557bc94c6
Miodrag Popović <miodragpopovic@stcable.net>
2019-07-22 10:20:40 +0200

    v8.3.4

--- karma.conf.js
+++ karma.conf.js
@@ -2,17 +2,61 @@
 
 // karma.conf.js
 module.exports = function(config) {
+  var fs = require('fs');
+
+  var isDocker;
+
+  function hasDockerEnv() {
+    try {
+      fs.statSync('/.dockerenv');
+      return true;
+    } catch (err) {
+      return false;
+    }
+  }
+
+  function hasDockerCGroup() {
+    try {
+      return fs.readFileSync('/proc/self/cgroup', 'utf8').indexOf('docker') !== -1;
+    } catch (err) {
+      return false;
+    }
+  }
+
+  function check() {
+    return hasDockerEnv() || hasDockerCGroup();
+  }
+
+  if (isDocker === undefined) {
+    isDocker = check();
+  }
+
+
 
   config.set({
-    browsers: ['Firefox'],
+    browsers: ['ChromeHeadlessNoSandbox'],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: isDocker? [
+          '--no-sandbox', // required to run without privileges in docker
+          '--user-data-dir=/tmp/chrome-test-profile',
+          '--disable-web-security'
+        ] : []
+      }
+    },
     frameworks: ['mocha'],
-    singleRun: true,
+    singleRun: false,
+    reporters: ['progress'],
+    logLevel: config.LOG_INFO,
+    //    port: 9876,  // karma web server port
+    autoWatch: false,
     files: [
-      './../../tests.js' // project root
+      '../../tests.js'
     ],
     plugins: [
       'karma-mocha',
-      'karma-firefox-launcher'
+      'karma-chrome-launcher',
     ]
   });
 
